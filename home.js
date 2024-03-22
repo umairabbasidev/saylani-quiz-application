dd7dbe2190537084b7df97e03fdc306bff8b61e6
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-22 17:12:20 +0500

    added quiz sections

---
+++ home.js
@@ -0,0 +1,64 @@
+// let quiztable = document.querySelector("#quiztable")
+
+
+// let quizTableData = [
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+//   {
+//     techName: "Python",
+//     subject: "1 Subject",
+//     btn: "Join",
+//   },
+// ];
+
+
+
+
+
+
+// quizTableData.map(({techName,subject,btn})=>{
+// return quiztable
+
+// })
+
+// // console.log("hell")
+
+// console.log(quiztable)

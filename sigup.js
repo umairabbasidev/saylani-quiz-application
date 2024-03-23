cf65c66a7156d8b50f8ae606e3418e0fcfffec27
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-03-23 08:23:14 +0500

    sign in

---
+++ sigup.js
@@ -0,0 +1,30 @@
+// submit.addEventListener("click", () => {
+//   let name = document.querySelector("#name").value;
+//   let age = document.querySelector("#age").value;
+//   let address = document.querySelector("#address").value;
+//   let email = document.querySelector("#email").value;
+//   let password = document.querySelector("#password").value;
+//   let submit = document.querySelector("#submit");
+//   let sendValue = {
+//     name: name,
+//     age: age,
+//     address: address,
+//     email: email,
+//     password: password,
+//   };
+
+//   let sendData = JSON.stringify(sendValue);
+//   localStorage.setItem("userDetail", sendData);
+//   let returnData = localStorage.getItem("userDetail");
+//   let convertReturnData = JSON.parse(returnData);
+//   console.log(sendData);
+//   console.log(convertReturnData);
+// });
+
+
+// </head>
+// <body>
+//     <!-- Add a button to trigger sign-in -->
+//     <button onclick="signIn()">Sign in with Google</button>
+// </body>
+// </html>
